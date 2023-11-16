8ff797cb64eb65a70b0ed6e79c59073a00146671
bcd6746@rit.edu <bcd6746@rit.edu>
2023-11-15 21:34:07 -0500

    created median point cloud

--- ImageInfo/ImageInfo.py
+++ ImageInfo/ImageInfo.py
@@ -8,13 +8,9 @@ class ImageInfo:
         return self.boundingbox
     def GetPosition(self):
         return self.position
-    def GetBackground(self):
-        return self.background
     def SetPosition(self,position):
         self.position = position
     def SetBoundingBox(self, boundingbox):
         self.boundingbox = boundingbox
     def SetVelocity(self, velocity):
         self.velocity = velocity
-    def SetBackground(self, background):
-        self.background = background
--- Project/mainPackage/main.py
+++ Project/mainPackage/main.py
@@ -8,6 +8,29 @@ import ImageInfo.ImageInfo as im
 
 def main(imgFiles, pointCloudfiles):
     imageInfo = []
+    pointClouds = []
+    x = 0
+    for filename in os.listdir(pointCloudfiles):
+        pointCloud = o3d.io.read_point_cloud(pointCloudfiles + "\\" + filename)
+        points = np.asarray(pointCloud.points)
+        pointClouds.append(points)
+        if (x >= 70):
+            break
+        x += 1
+    all_points = np.vstack(pointClouds)
+    max_points = max(len(pc) for pc in pointClouds)
+    median_cloud = np.zeros((max_points, 3))  # Assuming 3 dimensions (x, y, z)
+
+    for i in range(max_points):
+        points_at_index = np.array([pc[i] for pc in pointClouds if len(pc) > i])
+        median_point = np.median(points_at_index, axis=0) if len(points_at_index) > 0 else np.zeros(3)
+        median_cloud[i] = median_point
+
+    median_cloud = median_cloud[~np.all(median_cloud == 0, axis=1)]
+    fig = plt.figure(figsize=(24, 24))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(median_cloud[:,0], median_cloud[:,1], median_cloud[:,2], marker='o', s=20)
+    plt.show()
     for filename in os.listdir(pointCloudfiles):
         print(pointCloudfiles + "\\" + filename)
         vehicleInfo = createVehicleInfo((pointCloudfiles + "\\" + filename), imageInfo)
@@ -17,7 +40,7 @@ def FillVehics(img, imageInfo):
     clusters = ClusterLidar(img)
     for clusts in clusters:
         temp = im.ImageInfo(clusts)
-        #temp.SetPosition(clusts.)
+        # temp.SetPosition(clusts.)
         imageInfo.append(temp)
 
 
@@ -30,11 +53,9 @@ def ClusterLidar(file):
     print(len(points))
 
     clusters = np.array(0)
-
-    if (len(points) > 3):
-        clustering = DBSCAN(eps=.5, min_samples=10).fit(points)
-        cluster_labels = clustering.labels_
-        visualize_clusters(points, cluster_labels)
+    clustering = DBSCAN(eps=.5, min_samples=20).fit(points)
+    cluster_labels = clustering.labels_
+    visualize_clusters(points, cluster_labels)
     unique_labels = set(cluster_labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 
@@ -44,7 +65,7 @@ def ClusterLidar(file):
     return clusters
 
 
-def createVehicleInfo(file,imageInfo):
+def createVehicleInfo(file, imageInfo):
     fillVehics = FillVehics(file, imageInfo)
 
     # call functions to set each element of the vehic class
@@ -61,7 +82,7 @@ def visualize_clusters(points, cluster_labels):
     for cluster, color in zip(unique_labels, colors):
         cluster_points = points[cluster_labels == cluster]
         ax.scatter(cluster_points[:, 0], cluster_points[:, 1], cluster_points[:, 2], c=[color], marker='o', s=20)
-        #plt.show()
+        plt.show()
 
 
 if __name__ == "__main__":
