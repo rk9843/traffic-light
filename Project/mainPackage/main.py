615ea392f4d86391eab0df95dfeba44aab6a6e59
bcd6746@rit.edu <bcd6746@rit.edu>
2023-11-22 09:46:45 -0500

    subtracted median from every other

--- Project/mainPackage/main.py
+++ Project/mainPackage/main.py
@@ -2,16 +2,26 @@ import numpy as np
 import os
 import open3d as o3d
 from matplotlib import pyplot as plt
+from natsort import natsorted
 from sklearn.cluster import DBSCAN
+import natsort
 import ImageInfo.ImageInfo as im
 
 
-def main(imgFiles, pointCloudfiles):
+def main(imgFiles, pointCloudFiles):
     imageInfo = []
+    files = os.listdir(pointCloudFiles)
+    files = natsorted(files)
+    median_cloud = findMedianCloud(files, pointCloudFiles)
+    for filename in files:
+        print(pointCloudFiles + "\\" + filename)
+        vehicleInfo = createVehicleInfo((pointCloudFiles + "\\" + filename), imageInfo, median_cloud)
+
+def findMedianCloud(files,pointCloudFiles):
     pointClouds = []
     x = 0
-    for filename in os.listdir(pointCloudfiles):
-        pointCloud = o3d.io.read_point_cloud(pointCloudfiles + "\\" + filename)
+    for filename in files:
+        pointCloud = o3d.io.read_point_cloud(pointCloudFiles + "\\" + filename)
         points = np.asarray(pointCloud.points)
         pointClouds.append(points)
         if (x >= 70):
@@ -31,25 +41,25 @@ def main(imgFiles, pointCloudfiles):
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(median_cloud[:,0], median_cloud[:,1], median_cloud[:,2], marker='o', s=20)
     plt.show()
-    for filename in os.listdir(pointCloudfiles):
-        print(pointCloudfiles + "\\" + filename)
-        vehicleInfo = createVehicleInfo((pointCloudfiles + "\\" + filename), imageInfo)
+    return median_cloud
 
 
-def FillVehics(img, imageInfo):
-    clusters = ClusterLidar(img)
-    for clusts in clusters:
-        temp = im.ImageInfo(clusts)
+
+def FillVehics(img, imageInfo, median_cloud):
+    clusters = ClusterLidar(img,median_cloud)
+    #for clusts in clusters:
+        # temp = im.ImageInfo(clusts, median_cloud)
         # temp.SetPosition(clusts.)
-        imageInfo.append(temp)
+        #imageInfo.append(temp)
 
 
 # create clusters to count and track the cars
 # measure velocity from the movement of one frame to the next
 # compare based on the closest cluster to the position of the cluster in the previous frame
-def ClusterLidar(file):
+def ClusterLidar(file, median_cloud):
     pcd = o3d.io.read_point_cloud(file)
     points = np.asarray(pcd.points)
+    points = remove_matching_points(points, median_cloud)
     print(len(points))
 
     clusters = np.array(0)
@@ -61,19 +71,17 @@ def ClusterLidar(file):
 
     for cluster, color in zip(unique_labels, colors):
         clusters = points[cluster_labels == cluster]
-
     return clusters
 
 
-def createVehicleInfo(file, imageInfo):
-    fillVehics = FillVehics(file, imageInfo)
+def createVehicleInfo(file, imageInfo, median_cloud):
+    fillVehics = FillVehics(file, imageInfo, median_cloud)
 
     # call functions to set each element of the vehic class
 
-
 def visualize_clusters(points, cluster_labels):
     # Visualize clusters
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(24, 24))
     ax = fig.add_subplot(111, projection='3d')
 
     unique_labels = set(cluster_labels)
@@ -82,8 +90,28 @@ def visualize_clusters(points, cluster_labels):
     for cluster, color in zip(unique_labels, colors):
         cluster_points = points[cluster_labels == cluster]
         ax.scatter(cluster_points[:, 0], cluster_points[:, 1], cluster_points[:, 2], c=[color], marker='o', s=20)
-        plt.show()
+    plt.show()
+def remove_matching_points(pointcloud, mediancloud):
+    tolerance = 1e-1
+    rounded_pc1 = np.around(pointcloud, decimals=int(-np.log10(tolerance)))
+    rounded_pc2 = np.around(mediancloud, decimals=int(-np.log10(tolerance)))
 
+    pc1_set = {tuple(point) for point in rounded_pc1}
+    pc2_set = {tuple(point) for point in rounded_pc2}
+
+    # Find common points within the specified tolerance
+    matching_points = pc1_set.intersection(pc2_set)
+    matching_points = np.array(list(matching_points))
+
+    # Find indices of matching points in each point cloud
+    indices_pc1 = np.array([i for i, point in enumerate(rounded_pc1) if tuple(point) in matching_points])
+    #indices_pc2 = np.array([i for i, point in enumerate(rounded_pc2) if tuple(point) in matching_points])
+
+    # Remove matching points from both point clouds
+    filtered_pc1 = np.delete(pointcloud, indices_pc1, axis=0)
+
+
+    return filtered_pc1
 
 if __name__ == "__main__":
     # LOAD Folders
